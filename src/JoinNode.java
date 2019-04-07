9b1fdee89af04f178ef05164b4e59ca6064ce2ef
lin <50641445@qq.com>
2019-04-07 23:00:37 +0800

    11

---
+++ src/JoinNode.java
@@ -0,0 +1,31 @@
+import java.util.Map;
+
+/**
+ * 时间： 2019年04月07日  17时05分
+ * 作者： lcx
+ * 备注：
+ **/
+public class JoinNode {
+
+    private String key;  //关联外键
+
+    private Map<String, Object>  joinRes;//key 是 表名  value 是相应实体
+
+    public String getKey() {
+        return key;
+    }
+
+    public JoinNode setKey(String key) {
+        this.key = key;
+        return this;
+    }
+
+    public Map<String, Object> getJoinRes() {
+        return joinRes;
+    }
+
+    public JoinNode setJoinRes(Map<String, Object> joinRes) {
+        this.joinRes = joinRes;
+        return this;
+    }
+}
